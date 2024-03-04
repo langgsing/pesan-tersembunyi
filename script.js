9418a180a2f7421cb936254ba093190f842a5dc8
Ryujin <127178263+RyuGameDev@users.noreply.github.com>
2024-03-04 14:34:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+
 const cursor = document.querySelector(".cursor");
 window.addEventListener("mousemove", function(e){
-    cursor.style.left = (e.x-100)+"px";
-    cursor.style.top = (e.y-100)+"px";
+ cursor.style.left = (e.x-100)+"px";
+ cursor.style.top = (e.y-100)+"px";
+})
+
+window.addEventListener("touchmove", function(e){
+ cursor.style.left = (e.touches[0].clientX-100)+"px";
+ cursor.style.top = (e.touches[0].clientY-100)+"px";
 })
